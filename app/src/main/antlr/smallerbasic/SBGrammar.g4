a3adb607eef72072cad33f083ea7058d9157f4a5
Martino D'Adda <martino.dadda@studenti.unimi.it>
2023-10-13 18:50:57 +0200

    Boolean expressions and identifier lexing

--- app/src/main/antlr/smallerbasic/SBGrammar.g4
+++ app/src/main/antlr/smallerbasic/SBGrammar.g4
@@ -8,15 +8,26 @@ literal : Number
         | Bool
         ;
 
+booleanExpression : expression ('<='|'='|'<>'|'<'|'>'|'>=') expression
+                  | booleanExpression ('And'|'Or') booleanExpression
+                  | '(' booleanExpression ')'
+                  | Bool
+                  | Ident
+                  ;
+
 expression : expression ('/' | '*') expression
            | expression ('+' | '-') expression
            | '(' expression ')'
-           | literal
+           | String
+           | Number
+           | Ident
            ;
 
 fragment DIGIT : [0-9] ;
 fragment PRINTABLE : ~["] ;
-WS : [ \r\t\n]+ -> skip ;
+
+Ident  : [A-Za-z_][A-Za-z0-9_]* ;
+WS     : [ \r\t\n]+ -> skip ;
 Number : [+-]?(DIGIT+('.'DIGIT*)?|'.'DIGIT+)([eE][-+]?DIGIT+)? ;
 String : '"'PRINTABLE*'"' ;
 Bool   : 'true' | 'false' ;
--- app/src/test/java/smallerbasic/LexerTest.java
+++ app/src/test/java/smallerbasic/LexerTest.java
@@ -1,11 +1,9 @@
 package smallerbasic;
 
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,16 +35,19 @@ public class LexerTest {
                 .containsExactly("true", "false");
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "1 + 3 * 4,(expression (expression (literal 1)) + (expression (expression (literal 3)) * (expression (literal 4))))",
-            "1 / 3 - 4,(expression (expression (expression (literal 1)) / (expression (literal 3))) - (expression (literal 4)))",
-    })
-    void arithExprTest(String expr, String expected) {
-        SBGrammarLexer lexer = new SBGrammarLexer(CharStreams.fromString(expr));
-        SBGrammarParser parser = new SBGrammarParser(new CommonTokenStream(lexer));
+    @Test
+    void variableNameTest() {
+        SBGrammarLexer lexer = new SBGrammarLexer(CharStreams.fromString("a b _c C1A0"));
 
-        SBGrammarParser.ExpressionContext tree = parser.expression();
-        assertThat(tree.toStringTree(parser)).isEqualTo(expected);
+        assertThat(lexer.getAllTokens())
+                .extracting(Token::getText)
+                .containsExactly("a", "b", "_c", "C1A0");
     }
+
+    @Disabled
+    @Test
+    void variableNameMaximumLengthTest() {
+        assertThat(true).isEqualTo(false);
+    }
+
 }
