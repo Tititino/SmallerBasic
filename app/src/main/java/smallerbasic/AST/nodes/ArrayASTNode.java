1d9e5220cb95de9ad5d3e53294a9b66e67e20f89
Martino D'Adda <martino.dadda@studenti.unimi.it>
2023-10-25 05:04:13 +0200

    Multidimensional arrays

--- app/src/main/java/smallerbasic/AST/nodes/ArrayASTNode.java
+++ app/src/main/java/smallerbasic/AST/nodes/ArrayASTNode.java
@@ -3,23 +3,25 @@ package smallerbasic.AST.nodes;
 import org.jetbrains.annotations.NotNull;
 import smallerbasic.AST.ASTVisitor;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class ArrayASTNode extends AbstractASTNode implements ExpressionASTNode, VariableASTNode {
     private final @NotNull IdentifierASTNode name;
-    private final @NotNull ExpressionASTNode index;
+    private final @NotNull List<@NotNull ExpressionASTNode> indexes;
 
-    public ArrayASTNode(@NotNull IdentifierASTNode name, @NotNull ExpressionASTNode index) {
+    public ArrayASTNode(@NotNull IdentifierASTNode name, @NotNull List<@NotNull ExpressionASTNode> indexes) {
         this.name = name;
-        this.index = index;
+        this.indexes = indexes;
     }
 
     public @NotNull IdentifierASTNode getName() {
         return name;
     }
 
-    public @NotNull ExpressionASTNode getIndex() {
-        return index;
+    public @NotNull List<@NotNull ExpressionASTNode> getIndexes() {
+        return Collections.unmodifiableList(indexes);
     }
 
     @Override
@@ -27,20 +29,20 @@ public class ArrayASTNode extends AbstractASTNode implements ExpressionASTNode, 
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ArrayASTNode that = (ArrayASTNode) o;
-        return name.equals(that.name) && index.equals(that.index);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, index);
+        return name.equals(that.name) && indexes.equals(that.indexes);
     }
 
     @Override
     public String toString() {
         return "ArrayASTNode{" +
                 "name=" + name +
-                ", index=" + index +
+                ", index=" + indexes +
                 '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, indexes);
     }
 
     @Override
