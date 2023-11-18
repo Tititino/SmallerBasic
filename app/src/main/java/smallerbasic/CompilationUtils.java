5849af58d3b4abb25b282c28a7b5315427615ec7
Martino D'Adda <martino.dadda@studenti.unimi.it>
2023-11-18 03:17:49 +0100

    Modified CompilationUtils

--- app/src/main/java/smallerbasic/CompilationUtils.java
+++ app/src/main/java/smallerbasic/CompilationUtils.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 public class CompilationUtils {
     /**
      * Lex a source given as path to the file.
+     *
      * @param path The path of the file.
      * @return The {@link TokenStream} generated.
      * @throws IOException propagates any exception risen by the file handling.
@@ -30,25 +31,25 @@ public class CompilationUtils {
     private static @NotNull TokenStream lex(@NotNull CharStream chars) {
         SBGrammarLexer lexer = new SBGrammarLexer(chars);
         lexer.removeErrorListeners();
-        PrettyErrorListener listener = new PrettyErrorListener() {
+        BaseErrorListener listener = new BaseErrorListener() {
             @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-                hasFailed = true;
+            public void syntaxError(Recognizer<?, ?> recognizer,
+                                    Object offendingSymbol,
+                                    int line,
+                                    int charPositionInLine,
+                                    String msg,
+                                    RecognitionException e) {
                 System.err.println("*** LexError [" + line + ":"
                         + charPositionInLine + "]: " + msg);
-                underlineError(recognizer, (Token) offendingSymbol, line, charPositionInLine);
             }
         };
         lexer.addErrorListener(listener);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        if (listener.hasFailed())
-            throw new CompilationError("Lexing failed");
-        return tokens;
+        return new CommonTokenStream(lexer);
     }
 
     /**
      * Lex a source given as a string.
+     *
      * @param text The source.
      * @return The corresponding {@link TokenStream}.
      */
@@ -58,20 +59,13 @@ public class CompilationUtils {
 
     /**
      * Parse a {@link TokenStream} and return the {@link ParseTree}.
+     *
      * @param tokens The token stream.
      * @return {@code Optional.empty()} if there have been any parsing errors, the {@link ParseTree} otherwise.
      */
     public static @NotNull ParseTree parse(@NotNull TokenStream tokens) {
         SBGrammarParser parser = new SBGrammarParser(tokens);
-        PrettyErrorListener listener = new PrettyErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-                hasFailed = true;
-                System.err.println("*** ParseError [" + line + ":"
-                        + charPositionInLine + "]: " + msg);
-                underlineError(recognizer, (Token) offendingSymbol, line, charPositionInLine);
-            }
-        };
+        PrettyErrorListener listener = new PrettyErrorListener();
         parser.removeErrorListeners();
         parser.addErrorListener(listener);
         ParseTree tree = parser.program();
@@ -82,6 +76,7 @@ public class CompilationUtils {
 
     /**
      * Transforms the {@link ParseTree} to an AST.
+     *
      * @param tree The {@link ParseTree}, it must be well-formed. The behaviour otherwise is undefined.
      * @return The {@link ASTNode} root.
      */
@@ -91,14 +86,15 @@ public class CompilationUtils {
 
     /**
      * Given an {@link ASTNode}, a list of errors and a list of warnings, applies each check to the tree.
-     * @param tree The tree to be checked.
-     * @param errors A list of checks that the tree MUST pass.
+     *
+     * @param tree     The tree to be checked.
+     * @param errors   A list of checks that the tree MUST pass.
      * @param warnings A list of checks that the tree may not pass.
      * @return {@link Optional#empty()} if at least one check in {@code errors} fails.
      */
     public static @NotNull ASTNode check(@NotNull ASTNode tree,
-                                                   @NotNull List<Check> errors,
-                                                   @NotNull List<Check> warnings) {
+                                         @NotNull List<Check> errors,
+                                         @NotNull List<Check> warnings) {
         // directly using allMatch stops the checks at the first failed one
         List<Boolean> allPass = errors
                 .stream()
@@ -113,8 +109,9 @@ public class CompilationUtils {
 
     /**
      * Given a {@link Compiler} compiles the {@link ASTNode}.
+     *
      * @param tree The AST to compile.
-     * @param c A compiler.
+     * @param c    A compiler.
      * @return A string corresponding to the program represented by {@code tree}.
      */
     public static @NotNull String compile(@NotNull ASTNode tree, @NotNull Compiler c) {
